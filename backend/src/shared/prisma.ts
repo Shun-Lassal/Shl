ef1f8b54aa896022c221bd6da59377f31f58bed1
Shun <shun@quai13.com>
2025-11-24 11:14:46 +0100

    Prisma 7

--- backend/src/shared/prisma.ts
+++ backend/src/shared/prisma.ts
@@ -1,3 +1,27 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { defineConfig, env } from 'prisma/config';
+import 'dotenv/config'
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default defineConfig({
+    // the main entry for your schema
+    schema: 'prisma/schema.prisma',
+    // where migrations should be generated
+    // what script to run for "prisma db seed"
+    migrations: {
+        path: 'prisma/migrations',
+        seed: 'tsx shared/seeDefaultUser.ts',
+    },
+    // The database URL 
+    datasource: {
+        // Type Safe env() helper 
+        // Does not replace the need for dotenv
+        url: env('DATABASE_URL'),
+    },
+})
