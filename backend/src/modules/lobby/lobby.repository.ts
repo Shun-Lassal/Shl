8725bdda9bf0d1c6aafa36b4f2c9179f7f619ca0
Shun <shun@quai13.com>
2025-11-28 17:29:42 +0100

    Added Repo methods

--- backend/src/modules/lobby/lobby.repository.ts
+++ backend/src/modules/lobby/lobby.repository.ts
@@ -15,11 +15,39 @@ export class LobbyRepository {
 
     async addPlayerToLobby(id: string, playerId: string): Promise<Lobby> {
         return await prisma.lobby.update({where: {
-            id: id
+            id
         },
         data: {
-            players: playerId 
+            players: { push: playerId }
         }})
+    }
+
+    async removePlayerFromLobby(id: string, playerId: string): Promise<Lobby> {
+        const lobby: Lobby = await prisma.lobby.findUnique({
+            where: { id },
+            select: { players: true },
+        });
+
+        if (!lobby) {
+            throw 'Lobby not found'
+        }
+
+        if (lobby.players.length < 1) {
+            throw 'There is no player in the lobby'
+        }
+
+        const updatedPlayers: Array<string> = lobby.players.filter((player: string) => player !== playerId)
+
+        return await prisma.lobby.update({where: {
+            id
+        },
+        data: {
+            players: { set: updatedPlayers }
+        }})
+    }
+
+    async deleteLobby(id: string): Promise<Lobby | null> {
+        return await prisma.lobby.delete({where: { id }})
     }
 
 }
